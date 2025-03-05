6e726fb88d473b96f3779e953d06c0a1a988828c
zurychhh <134611895+zurychhh@users.noreply.github.com>
2025-03-05 15:07:59 +0100

    Delete src/components/src directory

--- src/components/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import MainSearch from "@/components/search/MainSearch";
-
-export default function Home() {
- return <MainSearch onRegionSelect={(region) => window.location.href = `/analyze/${region}`} />;
-}
